821b15325353f4f3851f56008707dbfb940486f1
Ilhomjon Buriev <35452989+ilhom0258@users.noreply.github.com>
2021-10-30 15:32:26 +0500

    Revert "Revert "merchandise-service created""

---
+++ src/MerchandiseService/Models/MerchItemIssueModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MerchandiseService.Models
+{
+    public class MerchItemIssueModel
+    {
+        public long MerchId { get; set; }
+        public int Quantity { get; set; }
+        public long EmployeeId { get; set; }
+    }
+}
